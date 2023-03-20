119604193783fc2b10bab6901f1099830b1524fb
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-20 13:37:30 -0700

    add more functionality Co-authored-by: John <john.lenz27@gmail.com> Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>

--- RecipeBox/Controllers/TagsController.cs
+++ RecipeBox/Controllers/TagsController.cs
@@ -7,5 +7,25 @@ using RecipeBox.Models;
 
 namespace RecipeBox.Controllers
 {
-    public class TagsController 
+    public class TagsController : Controllers
+    {
+        private readonly TagsController _dbs;
+
+        public TagsController(RecipeBoxContext db)
+        {
+            _dbs = db;
+        }
+        
+        public ActionResult Index()
+        {
+            return View (_dbs.Tags.ToList());
+        }
+
+        public ActionResult Create()
+  
+  
+  
+  
+  
+    }
 }
