d41016ffce6b724caa2db1b15008178f995914eb
Mitchell Gantz <mgantz22@icloud.com>
2023-03-21 17:12:11 -0700

    Add Identity

--- RecipeBox/Models/RecipeBoxContext.cs
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 
 namespace RecipeBox.Models
 {
-  public class RecipeBoxContext : DbContext
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Recipe> Recipes { get; set; }
     public DbSet<Tag> Tags { get; set; }
