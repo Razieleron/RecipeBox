b0f51af48a24dc07246c0ba6cdb8f0ea2d884b57
Mitchell Gantz <mgantz22@icloud.com>
2023-03-23 13:03:46 -0700

    Add new Migrations

---
+++ RecipeBox/Migrations/20230323172425_jannon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBox.Migrations
+{
+    /// <inheritdoc />
+    public partial class jannon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
