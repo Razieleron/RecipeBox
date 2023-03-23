3538bd7c8e67a687dc06ecc72f1599582ee74b17
Mitchell Gantz <mgantz22@icloud.com>
2023-03-22 17:55:07 -0700

    Auth migration

---
+++ RecipeBox/Migrations/20230322222351_tagAuth.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBox.Migrations
+{
+    /// <inheritdoc />
+    public partial class tagAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Tags",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_AspNetUsers_UserId",
+                table: "Tags",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_AspNetUsers_UserId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tags");
+        }
+    }
+}
